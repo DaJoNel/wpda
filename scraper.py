da5fe11eaf9f30c6fbdae620d2c416fdc325551d
David Nelson <dajonel@icloud.com>
2016-10-31 12:08:30 -0500

    Resolved HTML scraper handling of empty time values.

--- scraper.py
+++ scraper.py
@@ -9,18 +9,18 @@ import sys, requests, re, os, iso8601
 
 def scrape():
 	# Inital setup
-	venue = "hy[ -]?vee"
+	search = "hy[ -]?vee"
 	use_regex = 1
 	ignore_case = 1
 	max_lock = 6
 	country = 235
 
-	payload = {"vname": venue, "regex": use_regex, "ignorecase": ignore_case, "lock": max_lock, "country": country, "submit": "Search"}
+	payload = {"vname": search, "regex": use_regex, "ignorecase": ignore_case, "lock": max_lock, "country": country, "submit": "Search"}
 	#scrape = requests.post("https://db.slickbox.net/venues.php", data = payload)
+	scrape = open("/home/dnelson/Desktop/Hy-Vee.html").read()
 
 	# ------------------------------------- Parse the HTML Output -------------------------------------- #
 
-	scrape = open("/home/dnelson/Desktop/Hy-Vee.html").read()
 	# Find where the relevant data begins and ends in the HTML output
 	data = scrape[scrape.find('<tr id="link"'):scrape.rfind('</table>')]
 	#data = scrape.text[scrape.text.find('<tr id="link"'):scrape.text.rfind('</table>')]
@@ -33,41 +33,40 @@ def scrape():
 	for i in soup.find_all("a"):
 		i.insert(1, i.get("href"))
 
-	# Keep only the readable text and use a CSV-friendly delimiter
-	soup = soup.get_text('","')
+	# Keep only the readable text and arbitrarily use an uncommon delimiter
+	soup = soup.get_text('__')
 
 	# Clean up the output
-	soup = soup.replace('"," ', '","')	# Remove extra spaces
-	soup = soup.replace("(NULL)", "")	# Remove the random text
+	soup = soup.replace('__ ', '__')	# Remove extra spaces
+	soup = soup.replace("(NULL)", "")	# Remove the random text added earlier
 
 	# Split on the field delimiter
-	split = re.split('","', soup)
+	split = re.split('__', soup)
 
 	# Generate a 2D list of from the fields
-	venues = []
+	place = []
 	count = -1		# Start at -1 because it will auto-increment to 0 with the first iteration
 	for i in range(len(split)):
 		if i % 13 == 0:			# A new venue entry begins every 13 fields
-			venues.append([])
+			place.append([])
 			count+= 1
-		venues[count].append(split[i])
-
-
+		place[count].append(split[i])
 
 	# ------------------------------------- Export to the Database ------------------------------------- #
 
-	for row in venues:
+	for row in place:
 		ven_id = re.findall("(?!.*[venues=]).*", row[1])	# Extract the venue ID
 		
-		# The Django validator is not allowing a blank entry, so add the UNIX epoch time for now
-		if row[10] == "":
-			row[10] = "1970-01-01 00:00:00"
-		if row[11] == "":
-			row[11] = "1970-01-01 00:00:00"
-
 		# Generate a Python datetime object
-		row[10] = iso8601.parse_date(row[10])
-		row[11] = iso8601.parse_date(row[11])
+		if row[10] != "":
+			row[10] = iso8601.parse_date(row[10])
+		else:
+			row[10] = None		# If there is no time listed, do not attempt to parse
+
+		if row[11] != "":
+			row[11] = iso8601.parse_date(row[11])
+		else:
+			row[11] = None
 
 		# Add the row to the database
 		new = Place(venue_id=ven_id[0], name=row[0], permalink=row[1], lock_level=row[2], categories=row[3], number=row[4], street=row[5], city=row[6], state=row[7], country=row[8], updated_by=row[9], updated_on=row[10], user_report_on=row[11], is_residential=row[12])
