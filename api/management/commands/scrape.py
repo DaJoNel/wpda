7348d390ac075aada23c458a0edf0e085d3bf4ff
David Nelson <dajonel@icloud.com>
2016-12-09 01:41:25 -0600

    Major overhaul to scraper; bump version to 1.2.

--- api/management/commands/scrape.py
+++ api/management/commands/scrape.py
@@ -1,10 +1,12 @@
 #
 # Waze Place Discover & Audit (WPDA)
-# Version 1.1, 2016-12-07
+# Version 1.2, 2016-12-09
 # Copyright 2016 David Nelson. All rights reserved.
 #
 
+from bs4 import BeautifulSoup
 import sys, requests, re, os, iso8601
+
 from django.core.management.base import BaseCommand, CommandError
 from api.models import Place
 
@@ -28,58 +30,66 @@ class Command(BaseCommand):
 		# Find where the relevant data begins and ends in the HTML output
 		data = scrape.text[scrape.text.find('</thead>'):scrape.text.rfind('</table>')]
 
-		# Create a 2D list to store the parsed database
-		place = []
-		place.append([])
-		row = -1		# Start at -1 because it will auto-increment to 0 with the first iteration
-		col = 0
-		index = 0
+		# Fill empty fields with text to prevent BS from erasing them, then initialize BS
+		soup = BeautifulSoup(data, "html.parser")
 
-		# Parse through the data as follows:
-		# 1. Find each instance of <td> and note the index in the string
-		# 2. Starting at that position, look for the first instance of </td>
-		# 3. Store the string value between the two found index positions
-		# 4. Every 15 found fields, begin a new row denoting a new Place entry
-		while index < len(data):
-			try:
-				start = data.find("<td>", index)
-				if col == 15:			# A new venue entry begins every 15 fields
-					place.append([])
-					row+= 1
-					col = 0
-				end = data.index("</td>", start)
-				place[row].append(data[start+4:end])
-				index = end
-			except ValueError:
-				return ""
+		# Split the permalinks into separate fields in order to make the data useful
+		for a in soup.find_all("a"):
+			# Create three new elements (soon to be database fields)
+			permalink = soup.new_tag("td")
+			permalink.string = a.get("href")
+
+			name = soup.new_tag("td")
+			name.string = a.string
+
+			ven_id = soup.new_tag("td")
+			ven_id.string = re.findall("(?!.*[venues=]).*", permalink.string)[0]
+
+			# Add the new elements to the document tree
+			a.parent.append(ven_id)
+			a.parent.append(permalink)
+			a.parent.append(name)
+
+			# Clean up the original permalink
+			a.decompose()
+			ven_id.parent.unwrap()
+
+		# Generate a 2D list of the fields
+		place = []
+		count = 0
+		row = -1
+		for child in soup.find_all("td"):
+			if count % 16 == 0:			# A new venue entry begins every 16 fields
+				place.append([])
+				row+= 1
+				count = 0
+			if child.string is not None:
+				place[row].append(child.string.strip())
+			else:
+				place[row].append(child.string)		# Cannot strip a NoneType
+			count+= 1
 
 		# ------------------------------------- Export to the Database ------------------------------------- #
 
 		for row in place:
-			ven_id = re.findall("(?!.*[venues=]).*", row[1])	# Extract the venue ID
-
 			# Generate a Python datetime object for each date field
-			if row[10] != "":
-				row[10] = iso8601.parse_date(row[10])
-			else:
-				row[10] = None		# If there is no time listed, do not attempt to parse
+			if row[11] != None:
+				row[11] = iso8601.parse_date(row[11])
 
-			if row[12] != "":
-				row[12] = iso8601.parse_date(row[12])
-			else:
-				row[12] = None
+			if row[13] != None:
+				row[13] = iso8601.parse_date(row[13])
 
 			# We care whether there has been a place update request, not the date
-			if row[13] != "":
-				row[13] = True
+			if row[14] != None:
+				row[14] = True
 			else:
-				row[13] = False
+				row[14] = False
 
 			# Add the row to the database
-			new = Place(venueId=ven_id[0], name=row[0], permalink=row[1], lockLevel=row[2],
-			categories=row[3], number=row[4], street=row[5], city=row[6], state=row[7],
-			country=row[8], createdBy=row[9], createdOn=row[10], updatedBy=row[11],
-			updatedOn=row[12], updateRequest=row[13], isResidential=row[14])
+			new = Place(venueId=row[0], permalink=row[1], name=row[2], lockLevel=row[3],
+			categories=row[4], number=row[5], street=row[6], city=row[7], state=row[8],
+			country=row[9], createdBy=row[10], createdOn=row[11], updatedBy=row[12],
+			updatedOn=row[13], updateRequest=row[14], isResidential=row[15])
 			new.save()
 
 	def add_arguments(self, parser):
