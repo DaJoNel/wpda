b2592e33e3538daed205995db0c60386369433a5
David Nelson <dajonel@icloud.com>
2016-12-12 13:59:19 -0600

    Fixed an issue where Django admin was unable to set a cookie.

--- wpda/settings.py
+++ wpda/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 SECURE_BROWSER_XSS_FILTER = True
-SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
 #CSRF_COOKIE_HTTPONLY = True
 
@@ -157,8 +156,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, "static"),
-	'/var/www/wpda-server/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
